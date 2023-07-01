b1b948b8a9c8892d4731a14365db5c063fbe0a4f
Michelle Ndiangui <ndianguimichelle@gmail.com>
2023-07-02 01:08:37 +0300

    fixing an error

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 document.querySelector("#start").addEventListener('click', () => {
-    document.querySelector("span").innerText =     document.querySelector("#name").value
+  document.querySelector("form").classList.add("noDisp")
+  document.querySelector("section").classList.remove("noDisp")
+  document.querySelector("span").innerText = document.querySelector("#name").value
 
-    document.querySelector("form").classList.add("noDisp")
-    document.querySelector("section").classList.remove("noDisp")
 })
 
 function displayLists(arr, dest) {
